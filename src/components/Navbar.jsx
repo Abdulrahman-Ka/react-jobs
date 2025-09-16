a78188c4d90c2892b646b8cc395ecefc31f43c1f
Abdulrahman-Ka <abode.kalls@gmail.com>
2025-09-16 18:10:19 +0300

    first commit

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,43 @@
+import logo from "../assets/images/logo.png";
+
+const Navbar = () => {
+  return (
+    <nav className="bg-indigo-700 border-b border-indigo-500">
+      <div className="">
+        <div className="flex h-20 items-center justify-between">
+          <div className="flex flex-1 items-center justify-center md:items-stretch md:justify-start">
+            <a href="/index.html" className="flex shrink-0 items-center mr-4">
+              <img src={logo} alt="" className="h-10 w-auto" />
+              <span className="hidden md:block text-white text-2xl font-bold ml-2">
+                React Jobs
+              </span>
+            </a>
+            <div className="md:ml-auto">
+              <div className="flex space-x-2">
+                <a
+                  href="/index.html"
+                  className="text-white bg-black hover:bg-gray-900 hover:text-white rounded-md px-3 py-2"
+                >
+                  Home
+                </a>
+                <a
+                  href="/index.html"
+                  className="text-white  hover:bg-gray-900 hover:text-white rounded-md px-3 py-2"
+                >
+                  Jobs
+                </a>{" "}
+                <a
+                  href="/index.html"
+                  className="text-white  hover:bg-gray-900 hover:text-white rounded-md px-3 py-2"
+                >
+                  Add Job
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </nav>
+  );
+};
+export default Navbar;
